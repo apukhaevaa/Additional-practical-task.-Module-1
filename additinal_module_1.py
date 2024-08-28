98dcea660bfef9d31c588a37f2b7b99e40e97f89
apukhaevaa <apukhaevaa@gmail.com>
2024-08-28 13:35:31 +0300

    Update additinal_module_1.py

--- additinal_module_1.py
+++ additinal_module_1.py
@@ -7,5 +7,5 @@ grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 students_sorted = sorted(students)
 print(students_sorted)
-gpa = {student: sum(grades[i]) / len(grades[i]) for i, student in enumerate(students_sorted)}
+gpa = {student: sum(grades[t]) / len(grades[t]) for t, student in enumerate(students_sorted)}
 print(gpa)
